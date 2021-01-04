dd3ba31dfe2ae3bd5fbf76e650f132ee774428a2
JohanChataigne <Johan.Chataigner@enseirb-matmeca.fr>
2021-01-04 18:18:19 +0100

    fix issues in preprocessing module and use it

--- preprocessing.py
+++ preprocessing.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import re
+import os
 import spacy
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
@@ -12,10 +13,14 @@ class Preprocessor:
     
     def __init__(self, dataset):
         
+        os.system('python -m spacy download fr_core_news_md')
+        self.nlp = spacy.load('fr_core_news_md')
+        
         self.sentences = list(dataset['sentence'])
         self.intents = list(dataset['intent'])
         self.clean_text = []
         self.vectorized_text = []
+        self.padded_sentences = []
         
         # Keep track of the intents of the removed sentences during preprocessing
         self.intents_to_remove = []
@@ -26,13 +31,13 @@ class Preprocessor:
         for i, s in enumerate(self.sentences):
             # remove special characters
             clean_sentence = re.sub(r'[^ A-Za-z0-9éèàêî€]', '', s)
-            doc_s = nlp(clean_sentence)
+            doc_s = self.nlp(clean_sentence)
             
             # remove determiners (i.e. pos = 90 or pos_ = 'DET')
             remaining_words = list(filter(lambda x: x.pos != 90, doc_s))
             # build new doc object
             str_s = " ".join(list(map(lambda x: x.text, remaining_words)))
-            final_sentence = nlp(str_s)
+            final_sentence = self.nlp(str_s)
 
             # Exclude empty sentences
             if len(final_sentence) > 0: self.clean_text.append(final_sentence)
@@ -49,7 +54,7 @@ class Preprocessor:
 
     # Add padding to sentences to have same size datas
     def pad(self):
-        return pad_sequences(self.vectorized_text, dtype='float32', padding='post')
+        self.padded_sentences = pad_sequences(self.vectorized_text, dtype='float32', padding='post')
 
     # Apply the whole pipeline to input sentences and return them as numpy array object
     def preprocess_sentences(self):
@@ -76,7 +81,7 @@ class Preprocessor:
     # Apply one hot encoding to all the intents
     def preprocess_intents(self):
         self.clean_intents()
-        self.preprocessed_intents = np.asarray(list(map(intent2vec, self.intents)))
+        self.preprocessed_intents = np.asarray(list(map(self.intent2vec, self.intents)))
         
         
     # Apply whole preprocessing to dataset
