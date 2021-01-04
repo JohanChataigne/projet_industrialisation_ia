695ebec53d0eee5ae281e412be6b856693eafe29
JohanChataigne <Johan.Chataigner@enseirb-matmeca.fr>
2021-01-04 15:38:42 +0100

    add preproessing python module

---
+++ preprocessing.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import re
+import spacy
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# List of all intents in the same order as the model's output
+intents = ["find-train", "irrelevant", "find-flight", "find-restaurant", "purchase", "find-around-me", "provide-showtimes", "find-hotel"]
+    
+class Preprocessor:
+    
+    def __init__(self, dataset):
+        
+        self.sentences = list(dataset['sentence'])
+        self.intents = list(dataset['intent'])
+        self.clean_text = []
+        self.vectorized_text = []
+        
+        # Keep track of the intents of the removed sentences during preprocessing
+        self.intents_to_remove = []
+    
+    # Remove some special characters and split sentences
+    def clean(self):
+
+        for i, s in enumerate(self.sentences):
+            # remove special characters
+            clean_sentence = re.sub(r'[^ A-Za-z0-9éèàêî€]', '', s)
+            doc_s = nlp(clean_sentence)
+            
+            # remove determiners (i.e. pos = 90 or pos_ = 'DET')
+            remaining_words = list(filter(lambda x: x.pos != 90, doc_s))
+            # build new doc object
+            str_s = " ".join(list(map(lambda x: x.text, remaining_words)))
+            final_sentence = nlp(str_s)
+
+            # Exclude empty sentences
+            if len(final_sentence) > 0: self.clean_text.append(final_sentence)
+            else: self.intents_to_remove.append(i)
+
+    # Transform words into their vector representation
+    def vectorize(self):
+
+        null_vector = np.zeros(300)
+
+        for s in self.clean_text:
+            vects = [w.vector if w.has_vector else null_vector for w in s]
+            self.vectorized_text.append(vects)
+
+    # Add padding to sentences to have same size datas
+    def pad(self):
+        return pad_sequences(self.vectorized_text, dtype='float32', padding='post')
+
+    # Apply the whole pipeline to input sentences and return them as numpy array object
+    def preprocess_sentences(self):
+        self.clean()
+        self.vectorize()
+        self.pad()
+        self.preprocessed_sentences = np.asarray(self.padded_sentences)
+
+    # One hot encode one intent (take string representation of the label)
+    def intent2vec(self, intent):
+        assert intent in intents
+
+        idx = intents.index(intent)
+        vec = np.zeros(len(intents))
+        vec[idx] = 1
+        return vec
+    
+    # Remove intents of removed sentences
+    def clean_intents(self):
+        # Reverse to remove the right elements (no error due to index shifting)
+        for idx in reversed(self.intents_to_remove):
+            self.intents.pop(idx)
+        
+    # Apply one hot encoding to all the intents
+    def preprocess_intents(self):
+        self.clean_intents()
+        self.preprocessed_intents = np.asarray(list(map(intent2vec, self.intents)))
+        
+        
+    # Apply whole preprocessing to dataset
+    def preprocess(self):
+        self.preprocess_sentences()
+        self.preprocess_intents()
+        
+        # TODO build preprocessed dataset object
+        
+        
+    
