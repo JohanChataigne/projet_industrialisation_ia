fa240d746542a1c1d6ab90ea8a682c2afca83f7d
JohanChataigne <Johan.Chataigner@enseirb-matmeca.fr>
2021-01-06 17:16:43 +0100

    add swagger docs

---
+++ api.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flasgger import Swagger, swag_from
+from preprocessing import preprocess_sentence
+from tensorflow import keras
+
+
+
+app = Flask(__name__)
+swagger = Swagger(app)
+
+@app.route('/')
+def hello():
+    return "Welcome to Intent Classification"
+
+@app.route('/api/')
+@app.route('/api/docs/')
+def show_docs():
+    return "Documentation"
+
+@app.route('/api/intent/')
+def usage():
+    return "Add a sentence to your request to get a prediction! \n Example: '/api/intent/Hello world!'"
+
+@app.route('/api/intent/<sentence>/')
+@swag_from('specs.yaml')
+def predict(sentence):
+    
+    model = keras.models.load_model('./models/model_v1')
+    x = preprocess_sentence(sentence)
+    prediction = model.predict(x)
+    return str(prediction)
+    
+
+if __name__ == "__main__":
+    
+    app.run(debug=True)
